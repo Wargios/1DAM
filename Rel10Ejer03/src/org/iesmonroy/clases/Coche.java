2735fe29fd99aea31afde61c34790f91a53daf8e
alumno <alumno@1smr-pc05>
2016-04-01 08:43:12 +0200

    r2

--- Rel10Ejer03/src/org/iesmonroy/clases/Coche.java
+++ Rel10Ejer03/src/org/iesmonroy/clases/Coche.java
@@ -6,8 +6,8 @@ public class Coche extends Vehiculo{
 	private String combustible;
 	
 	public Coche(String matricula, String Gama, String combustible) {
-		
 		super(matricula, Gama, combustible);
+		
 	}
 	
 	
